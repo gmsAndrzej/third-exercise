c2892fc13650b5c2bb04d19b3ad3767aadb4f952
Andrzej Stoklosa <gmsandrzejstoklosa@gmail.com>
2022-05-05 10:07:44 +0200

    added js control

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-console.log ("witaj ") ;
+console.log("witaj kolego ");
+let button = document.querySelector(".button");
+let types = document.querySelector(".types");
+
+button.addEventListener("click", () => {
+
+    types.classList.toggle("dark");
+
+
+
+
+});
